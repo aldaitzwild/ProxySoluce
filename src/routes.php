f2526c30a819280b0466fe42e9d512606eda951f
Valentin <valentingaudin@gmail.com>
2021-11-03 17:39:28 +0100

    Save the form for the inscription, and its done

--- src/routes.php
+++ src/routes.php
@@ -8,6 +8,9 @@
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
     '' => ['HomeController', 'index',],
+    'login' => ['RegisterController', 'login'],
+    'inscription' => ['RegisterController', 'add'],
+    'welcome' => ['RegisterController', 'welcome'],
     'items' => ['ItemController', 'index',],
     'items/edit' => ['ItemController', 'edit', ['id']],
     'items/show' => ['ItemController', 'show', ['id']],

54673aba105c486ec08ace3387f92fb022c811bc
Valentin <valentingaudin@gmail.com>
2021-11-05 09:28:07 +0100

    Save the start for the login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,8 +2,26 @@
 
 namespace App\Controller;
 
-use App\ModelLoginManager;
+use App\Model\LoginManager;
 
 class LoginController extends AbstractController
 {
+    public function login()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $infos = array_map('trim', $_POST);
+            $infos = array_map('htmlentities', $infos);
+            $infos = array_map('stripslashes', $infos);
+            $login = new LoginManager();
+            if ((($login->checkUser($infos)) == true) && ($login->checkPass($infos)) == true) {
+                echo "okay" ;
+            } else {
+                echo "not ok";
+            }
+                var_dump($_POST);
+                var_dump($infos);
+                //header('location:/welcome');
+        }
+        return $this->twig->render('Login/login.html.twig');
+    }
 }
--- src/Model/LoginManager.php
+++ src/Model/LoginManager.php
@@ -4,4 +4,21 @@ namespace App\Model;
 
 class LoginManager extends AbstractManager
 {
+    public const TABLE = 'Login';
+
+    public function checkUser(array $user)
+    {
+        $statement = $this->pdo->prepare("SELECT * FROM " . self::TABLE . " WHERE `user` = :user");
+        $statement->bindValue('user', $user['user'], \PDO::PARAM_STR);
+        $statement->execute();
+        return $statement->fetch();
+    }
+
+    public function checkPass(array $pass)
+    {
+        $statement = $this->pdo->prepare("SELECT * FROM " . self::TABLE . " WHERE `pass` = :pass");
+        $statement->bindValue('pass', password_hash($pass['pass'], PASSWORD_BCRYPT), \PDO::PARAM_STR);
+        $statement->execute();
+        return $statement->fetch();
+    }
 }

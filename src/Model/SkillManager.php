aff3cfbd734e01cc73dff9f19f65e904eea42686
Valentin <valentingaudin@gmail.com>
2021-11-07 21:43:23 +0100

    Pause the dev of login

--- src/Model/SkillManager.php
+++ src/Model/SkillManager.php
@@ -7,4 +7,13 @@ use PDO;
 class SkillManager extends AbstractManager
 {
     public const TABLE = 'skill';
+
+    /** Insert skill off a person in database */
+    public function insert(array $choose): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`) VALUES (:name)");
+        $statement->bindValue('title', $choose['s.name'], \PDO::PARAM_STR);
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
 }

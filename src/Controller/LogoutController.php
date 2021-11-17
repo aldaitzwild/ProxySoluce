86fd9d9bf8d9adc81f13d4e8b22be579fc2b4fb2
Michelhof1978 <michel.hof@hotmail.fr>
2021-11-17 17:23:25 +0100

    final

--- src/Controller/LogoutController.php
+++ src/Controller/LogoutController.php
@@ -6,9 +6,9 @@ class LogoutController extends AbstractController
 {
     public function logout()
     {
-    if (isset($_SESSION['userLogged'])) { // Si connecté on déconnecte et on te redirige vers une page.
-        session_destroy();
-        header("Location:/");
-        }
+        if (isset($_SESSION['userLogged'])) { // Si connecté on déconnecte et on te redirige vers une page.
+            session_destroy();
+            header("Location:/");
+            }
     }
 }

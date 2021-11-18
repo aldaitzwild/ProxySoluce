28986475f22a19de203eedde76458d75570aa651
Anthony69600 <a.verchere69@gmail.com>
2021-11-18 15:03:54 +0100

    route_and_empty_description1

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@
  * Date: 11/10/17
  * Time: 14:01
  */
+
 session_cache_limiter('private, must-revalidate');
 session_cache_expire(60);
 session_start();
@@ -16,5 +17,4 @@ if (getenv('ENV') === false) {
     require_once __DIR__ . '/../config/db.php';
 }
 require_once __DIR__ . '/../config/config.php';
-
 require_once __DIR__ . '/../src/routing.php';
